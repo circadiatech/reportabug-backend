1b163bd097bb5902f6ee13cfed0b1f37dc6138c8
Muhammad Iqbal Trainer <iqbaltrainer3@GMAIL.COM>
2021-10-30 18:36:01 +0500

    create Delete a Task

--- project/__init__.py
+++ project/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from database import db
 
 
 app = Flask(__name__)
@@ -12,9 +13,24 @@ ma = Marshmallow(app)
 
 import controllers.notes
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+@app.route('/project/<id>', methods=['DELETE'])
+def delete(self):
+    data = RequestCacheControl.get_json()
+    task = data["task"]
+    obj = Task_Model.find_by_name(task)
+    
+    if obj is None:
+            return make_response(jsonify({
+                "Msg": "Task Does not exist",
+                "Status": 404
+            }),404)
+        
+    obj.delete_from_db()
+    
+    return make_response(jsonify({
+            "Msg": "Task Deleted",
+            "Status": 202
+        }),202)
 
 if __name__ == '__main__':
 	app.run()
