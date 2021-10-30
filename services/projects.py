fd579af0e7b688f370d4f2e36f7c12936da4d782
wajahat <wajahat@circadia.health>
2021-10-30 15:40:41 +0500

    Added utility function

--- services/projects.py
+++ services/projects.py
@@ -5,3 +5,8 @@ from models.project import Project, project_schema, projects_schema
 def fetch_all_projects():
     projects = Project.query.all()
     return projects_schema.dump(projects)
+
+
+def fetch_project(id):
+    project = Project.query.filter_by(id=id).first_or_404()
+    return project_schema.dump(project)
